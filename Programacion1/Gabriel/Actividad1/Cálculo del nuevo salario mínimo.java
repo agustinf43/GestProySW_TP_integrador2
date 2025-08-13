5b8b9a41a92262f0ca132263de5462393b9278d7
gabir <gabirivero738|@>
2025-08-12 21:47:19 -0300

    commit

---
+++ Programacion1/Gabriel/Actividad1/Cálculo del nuevo salario mínimo.java
@@ -0,0 +1,14 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        final double SALARIO_ACTUAL = 84512.0;
+
+        double salarioSindicato = SALARIO_ACTUAL + (SALARIO_ACTUAL * 0.40);
+        double salarioGobierno = SALARIO_ACTUAL + (SALARIO_ACTUAL * 0.20);
+
+        double diferencia = salarioSindicato - salarioGobierno;
+
+        System.out.println("Salario con aumento del sindicato: $" + salarioSindicato);
+        System.out.println("Salario con aumento del gobierno: $" + salarioGobierno);
+        System.out.println("Diferencia: $" + diferencia);
+    }
+}
